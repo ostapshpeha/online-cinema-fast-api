0a06a03ecac3cc934cf50561b177b2f62b3d0eae
Ann Riabykina <anny.r20.1994@gmail.com>
2026-02-05 17:56:55 +0200

    Fixed Ruff

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.interactions.router import router as interaction_router
 
 app = FastAPI(title="Online Cinema API")
 app.include_router(auth_router, prefix="/api/v1")
-app.include_router(interactions_router, prefix="/api/v1")
+app.include_router(interaction_router, prefix="/api/v1")
 
 
 @app.get("/")
