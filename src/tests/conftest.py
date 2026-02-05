4f0a06cbfa37c20fa69282874d617685ce27077e
Ostap Shp <stark171705@gmail.com>
2026-02-05 16:13:38 +0200

    Added CI git hub | Initialised tests

---
+++ src/tests/conftest.py
@@ -0,0 +1,29 @@
+import asyncio
+import pytest
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from src.core.database import Base
+from src.core.config import settings
+from typing import AsyncGenerator
+
+
+test_engine = create_async_engine(settings.DATABASE_URL_ASYNC, echo=False)
+TestingSessionLocal = async_sessionmaker(test_engine, expire_on_commit=False)
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database():
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+@pytest.fixture
+async def db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with TestingSessionLocal() as session:
+        yield session
