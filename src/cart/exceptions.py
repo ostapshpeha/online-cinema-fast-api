35de5026cea681bdfc3448abb457576ee18d2144
Andrii Yermolenko <vakt159@gmail.com>
2026-02-06 13:23:15 +0200

    Add custom exceptions for cart

---
+++ src/cart/exceptions.py
@@ -0,0 +1,18 @@
+
+class CartBaseException(Exception):
+    pass
+
+class MovieAlreadyPurchasedException(CartBaseException):
+    pass
+
+class MovieAlreadyInCartException(CartBaseException):
+    pass
+
+class MovieNotInCartException(CartBaseException):
+    pass
+
+class CartIsNotExistException(CartBaseException):
+    pass
+
+class CartIsEmptyException(CartBaseException):
+    pass
